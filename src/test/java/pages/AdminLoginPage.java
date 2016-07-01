cbd941b7d4f7969ae37721c41bf03206b37bf2c3
iahmedgov <iahmed@govolution.com>
2016-07-01 16:50:10 -0400

    made changes to Login functionality

--- src/test/java/pages/AdminLoginPage.java
+++ src/test/java/pages/AdminLoginPage.java
@@ -13,7 +13,7 @@ public class AdminLoginPage {
 	By adminuserID = By.name("user");
 	By adminPassword = By.name("password");
 	By adminSubmit = By.cssSelector("input.button2");
-//userid and password to login	 
+//	userid and password to login HAHA 
 	String pUserid;
 	String pPassword;
 	
